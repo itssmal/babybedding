6c5902d1f741926d22945eef2727b58027684f0d
Sasha <wwesasha12@gmail.com>
2020-08-12 12:11:10 +0300

    added mailgun

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
-const nodemailer = require("nodemailer");
-const hbs = require('nodemailer-express-handlebars')
+const nodemailer = require('nodemailer');
+const mailgun = require('nodemailer-mailgun-transport')
+const hbs = require('handlebars')
 const path = require('path')
+const fs = require("fs")
 
 const categoryRoute = require('./routes/client-routes/category')
 const orderRoute = require('./routes/client-routes/order')
@@ -50,38 +52,38 @@ app.use('/api/admin-category', adminCategoryRoute)
 app.use('/api/admin-order', adminOrderRoute)
 app.use('/api/admin-position', adminPositionRoute)
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: "babybedding.ua@gmail.com",
-        pass: "adminBabyBed"
-    }
-});
+const emailTemplateSource = fs.readFileSync(path.join(__dirname, "/main.handlebars"), "utf8")
 
-transporter.use('compile', hbs({
-    viewPath: './'
-}))
+const auth = {
+    auth: {
+        api_key: keys.mailApi,
+        domain: keys.mailDom
+    }
+}
+
+const transporter = nodemailer.createTransport(mailgun(auth))
+
+const template = hbs.compile(emailTemplateSource)
 
 app.post('/sendMailToUser', function(req, res, next) {
+    const htmlToSend = template({
+        positions: req.body.positions,
+        sum: req.body.sum,
+        userName: req.body.userName,
+        userPhone: req.body.userPhoneNumber,
+        area: req.body.area,
+        city: req.body.city,
+        department: req.body.department,
+    })
     transporter.sendMail({
-        from: '"Baby Bedding" babybedding.ua@gmail.com', // sender address
-        to: `its.ssmal@gmail.com, ${req.body.userEmail}`,// list of receivers
+        from: 'Baby Bedding order@babybedding.com.ua', // sender address
+        to: `babybedding.ua@gmail.com, ${req.body.userEmail}`,// list of receivers
         subject: `Дякуємо! Ваше Замовлення на Baby Bedding`, // Subject line
-        template: 'main',
-        context: {
-            positions: req.body.positions,
-            sum: req.body.sum,
-            userName: req.body.userName,
-            userPhone: req.body.userPhoneNumber,
-            area: req.body.area,
-            city: req.body.city,
-            department: req.body.department,
-        }
+        html: htmlToSend
     }, function (err, response) {
         if (err) {
             res.send(err)
+            console.log(err)
         } else {
             res.send('good email')
         }
--- config/keys.prod.js
+++ config/keys.prod.js
@@ -1,4 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    jwt: process.env.JWT
+    jwt: process.env.JWT,
+    mailApi: process.env.MAIL_API,
+    mailDom: process.env.MAIL_DOM
 }
