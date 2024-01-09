9820ad761b77ca3b013227daef8bc5ddec1fff3e
Sasha <wwesasha12@gmail.com>
2024-01-09 14:51:10 +0200

    Update Nova Poshta API key for prod

--- client/ng-client/src/environments/environment.prod.ts
+++ client/ng-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiEndPoint: 'https://api.novaposhta.ua/v2.0/json/',
-  apiKey: 'd2997b2942fe39a5809ce439ccbdcaef'
+  apiKey: 'ab924ef68f35af8f19df7bd79c9cf969'
 };
