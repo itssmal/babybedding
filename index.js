8b188298ee3b462f9da7a3e1a6c44a10443c6a3c
Oleksandr Smal <oleksandr.smal@match2one.com>
2025-01-17 10:30:55 +0200

    Implement HTTP redirect to HTTPS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 const fs = require("fs")
+const http = require('http');
 const https = require('https');
 
 const port = process.env.PORT || 5000
@@ -8,6 +9,10 @@ const options = {
     cert: fs.readFileSync('/etc/letsencrypt/live/babybedding.store/fullchain.pem'),
 };
 
+http.createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+}).listen(80, () => console.log('HTTP server running on port 80 (redirecting to HTTPS)'));
 const httpsServer = https.createServer(options, app);
 
 httpsServer.listen(port, () => console.log(`Server started at ${port}`))
