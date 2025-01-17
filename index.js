a9fef1eefeea64852d8d568b1cf574225eead483
Oleksandr Smal <oleksandr.smal@match2one.com>
2025-01-17 10:10:35 +0200

    Implement HTTPS

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const app = require('./app')
+const fs = require("fs")
+const https = require('https');
+
 const port = process.env.PORT || 5000
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/babybedding.store/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/babybedding.store/fullchain.pem'),
+};
 
+const httpsServer = https.createServer(options, app);
 
-app.listen(port, () => console.log(`Server started at ${port}`))
-
+httpsServer.listen(port, () => console.log(`Server started at ${port}`))
